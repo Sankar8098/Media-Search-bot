fba3af9f65de331003d8404de46839a025251a26
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-11-11 10:08:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -6,9 +6,9 @@ id_pattern = re.compile(r'^.\d+$')
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
 USER_SESSION = environ.get('USER_SESSION', 'User_Bot')
-API_ID = int(environ['23990433'])
-API_HASH = environ['e6c4b6ee1933711bc4da9d7d17e1eb20']
-BOT_TOKEN = environ['5992501587:AAHdDLpHnleGcpCQUizT1efHLhDNYUrtuYA']
+API_ID = int(environ.get('API_ID', '23990433'))
+API_HASH = environ.get('API_HASH', 'e6c4b6ee1933711bc4da9d7d17e1eb20')
+BOT_TOKEN = environ.get('BOT_TOKEN', "5992501587:AAHdDLpHnleGcpCQUizT1efHLhDNYUrtuYA")
 USERBOT_STRING_SESSION = environ.get('USERBOT_STRING_SESSION')
 
 # Bot settings
