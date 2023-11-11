86a6388e543f958e1105265c9df68931866a45e2
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-11-11 10:24:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', ' -1002145600271').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get(''-1002145600271')
+auth_channel = environ.get('AUTH_CHANNEL')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 
 # MongoDB information
